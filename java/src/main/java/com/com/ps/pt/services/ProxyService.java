b6f63ef5083d67bec9d3acd95185fb986d94d4e4
Chris Buckley <cbuckley@puresrc.com>
2011-10-14 14:24:00 -0700

    Source for the Java module

---
+++ java/src/main/java/com/com/ps/pt/services/ProxyService.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) Pure Source, LLC All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.com.ps.pt.services;
+
+/**
+ * Author: cnbuckley
+ * Date: 10/14/11
+ * Time: 2:22 PM
+ */
+public interface ProxyService {
+}
